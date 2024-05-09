8c56c4de7963a12ac8f9a91a8bbca9b13de2aebd
CaioGGR <caioggr@gmail.com>
2024-05-09 19:27:24 -0300

    Adicionando aulas e exercicios

---
+++ ExerciciosWhile/Ex3.js
@@ -0,0 +1,6 @@
+let contador = prompt('Digite um número para começarmos a contagem regressiva')
+
+while (contador >= 0) {
+  alert(contador)
+  contador--
+}
---
+++ ExerciciosWhile/Ex4.js
@@ -0,0 +1,6 @@
+let numero = prompt('Digite um número para começarmos a contagem progressiva')
+let contador = 0
+while (contador <= numero) {
+  alert(contador)
+  contador++
+}
